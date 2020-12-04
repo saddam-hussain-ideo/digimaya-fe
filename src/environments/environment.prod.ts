b98753c06767b183c70fd4e6e388796d20c60ff3
rana_fahad15 <rana.fahad@ideofuzion.com>
2020-12-04 18:24:18 +0500

    change env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BaseUrl: "http://api.fruture.org/api/",
+  BaseUrl: "http://coin.piptleapp.store:3000/api/",
   documentUrl: "https://fruture.org/terms-of-use.html",
   privacyPolicy:"https://fruture.org/privacypolicy.html",
   defaultLanguage: 'es'
