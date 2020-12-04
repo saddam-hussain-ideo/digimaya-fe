426ea6586927932d46ac3cabccbad65e99a2de4c
rana_fahad15 <rana.fahad@ideofuzion.com>
2020-12-04 18:19:11 +0500

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BaseUrl: "https://api.fruture.org/api/",
+  BaseUrl: "http://api.fruture.org/api/",
   documentUrl: "https://fruture.org/terms-of-use.html",
   privacyPolicy:"https://fruture.org/privacypolicy.html",
   defaultLanguage: 'es'
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BaseUrl: "https://sapi.fruture.org/api/",
+  BaseUrl: "http://api.fruture.org/api/",
   documentUrl: "https://fruture.org/terms-of-use.html",
   privacyPolicy:"https://fruture.org/privacypolicy.html",
   defaultLanguage: 'es'
