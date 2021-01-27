2a0b6eaba6f7f88b7cd35c67ecb82caca5980328
awaiss-iftikhar <awais.iftikhar@ideofuzion.com>
2021-01-27 16:18:05 +0500

    update url

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  BaseUrl: "https://api.mypiptles.com/api/",
+  BaseUrl: "http://coin.piptleapp.store:3000/api/",
+  // BaseUrl: "https://api.mypiptles.com/api/",
   documentUrl: "https://fruture.org/terms-of-use.html",
   privacyPolicy:"https://fruture.org/privacypolicy.html",
   captchaKey: '6LcY8zIaAAAAAJrc0THvOOZyjIdJKLYAl3tHnITp',
